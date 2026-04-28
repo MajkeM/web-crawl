1a5417a998f1024253dd146311fd7056db3a1524
MajkeM <156318301+MajkeM@users.noreply.github.com>
2026-04-28 08:56:18 +0200

    complete

---
+++ README.md
@@ -0,0 +1,11 @@
+you need to have installed node:
+you can install it here: https://nodejs.org/en
+
+you need these dependencies insatlled:
+axios -> npm install axios
+
+for normal web-crawl that scans the html file for any anchor tags use:
+node web-crawl.js
+
+for brute-force web that uses common.txt wordlist from secLists use:
+node web-crawl.js --bruteforce
--- web-crawl.js
+++ web-crawl.js
@@ -3,53 +3,36 @@ import * as cheerio from "cheerio";
 import fs from "fs";
 import readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
-
 const rl = readline.createInterface({ input, output });
 const url = await rl.question("Enter the URL (include the HTTP protocol) to crawl: ");
 console.log(`Crawling ${url}...`);
-
 const args = process.argv.slice(2);
-const bruteforce = args.includes("--bruteforce"); 
-
-
+const bruteforce = args.includes("--bruteforce");
 const response = await axios.get(url);
 const html = response.data;
-
 // ----------------------- web-crawl -----------------------
-if (!bruteforce){
-
-const $ = cheerio.load(html);
-const links:Array<string>|undefined = [];
-
-function isDuplicatedLink(link:string) : boolean {
-    if (link === "/"){
-        return true;
+if (!bruteforce) {
+    const $ = cheerio.load(html);
+    const links = [];
+    function isDuplicatedLink(link) {
+        if (link === "/") {
+            return true;
+        }
+        return links?.includes(link) || false;
     }
-    return links?.includes(link) || false;
-}
-
-
-$("a").each((index, element) => {
-
-    const link = ($(element).attr("href"));
-
-    if (link && !isDuplicatedLink(link)){
-        links.push(link);
-    }
-});
-
-
-links.map((link) => {
-    console.log(link);
-});
-
+    $("a").each((index, element) => {
+        const link = ($(element).attr("href"));
+        if (link && !isDuplicatedLink(link)) {
+            links.push(link);
+        }
+    });
+    links.map((link) => {
+        console.log(link);
+    });
 }
 // ----------------------- brute force -----------------------
-
-if (bruteforce){
-
+if (bruteforce) {
     const words = fs.readFileSync("common.txt", "utf-8").split("\n");
-
     for (const word of words) {
         try {
             const res = await axios.get(`${url}/${word}`);
